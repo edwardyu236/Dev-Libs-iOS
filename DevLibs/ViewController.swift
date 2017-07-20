c3d48a461eb7c16900abae1511b963af9208e078
Edward Yu <yedward@vmware.com>
2017-07-20 10:19:07 -0700

    Initial Commit

---
+++ DevLibs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevLibs
+//
+//  Created by Edward Yu US1070055 on 7/20/17.
+//  Copyright © 2017 vmware-yedward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
