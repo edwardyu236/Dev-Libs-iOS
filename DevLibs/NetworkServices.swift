2d1fa351982af34753c1abbdd4f488304fd0cab0
Edward Yu <yedward@vmware.com>
2017-07-20 16:38:41 -0700

    hackathon demo

---
+++ DevLibs/NetworkServices.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkServices.swift
+//  DevLibs
+//
+//  Created by Edward Yu US1070055 on 7/20/17.
+//  Copyright © 2017 vmware-yedward. All rights reserved.
+//
+
+import Foundation
---
+++ DevLibs/SentenceFormViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SentenceFormViewCell.swift
+//  DevLibs
+//
+//  Created by Edward Yu US1070055 on 7/20/17.
+//  Copyright © 2017 vmware-yedward. All rights reserved.
+//
+
+import UIKit
+
+class SentenceFormViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
